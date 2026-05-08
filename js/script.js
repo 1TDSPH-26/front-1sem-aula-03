361f0856eccc7fb7ee8d75ae4f64932e8d2dc10a
Gabrieo367 <gabrieo367@gmail.com>
2026-05-08 09:51:07 -0300

    aula 3

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//Declaração de variáveis com JS
+//3 tipos = var / let / const
+/** comentario de bloco  */
+
+//Declaração de var:
+//var nome = "Fiap";
+//console.log("A melhor faculdade : " + nome);
+
+//var nr1 = 10;
+//var nr2 = "5";
+//console.log("Resultado da soma : " + (nr1 + parseInt (nr2) ));
+
+// var  variavel = "Alê";
+// console.log(typeof variavel);
+
+// var  variavel = 100;
+// console.log(typeof variavel);
+
+// var  variavel = true;
+// console.log(typeof variavel);
+
+// var  variavel = null;
+// console.log(typeof variavel);
+
+// var  variavel = 'a';
+// console.log(typeof variavel);
+
+// var  variavel = [];
+// console.log(typeof variavel);
+
+// var  variavel = 100.456;
+// console.log(typeof variavel);
+
+// var  variavel = {};
+// console.log(typeof variavel);
+
+
+// let = 17;
+
+// const resultado = idade > 18 ? "Pode passear" : "Não pode passear"
+// //Utilizando template literals `´ com interpolação
+
+// console.log(`O resultado é que ele ${resultado.toLowerCase()}.`)
+
+var nome1 = "ALê";
+let nome2 = "Carlos";
+
+if(true){
+    //Redeclarando
+    var nome1 = "João";
+    let nome2 = "das Neves";
+}
+console.log(nome1);
+console.log(nome2);
