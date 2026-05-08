26fae9ff59a5e57646561c8a3ef97512eb506ec1
murilo-a-souza <sir.murilos@gmail.com>
2026-05-08 10:45:12 -0300

    feat : Ajustes e criação do script.js.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Olá mundo!");
+
+//variaveis
+var nome = "Fiap";
+
+//criando um ternário e mexendo com cálculos
+
+//template literals '' com interpolação ${}
+console.log(typeof nome);
